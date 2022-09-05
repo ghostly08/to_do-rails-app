ebda1eae1541202e3a5b96f595c635353bb30a16
ghostly08 <steven.produccionbcn@gmail.com>
2022-09-05 18:07:54 +0200

    link_to new task form from user show

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update] do
     resources :tasks
   end
-  root to: "pages#home"
+  root to: "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
