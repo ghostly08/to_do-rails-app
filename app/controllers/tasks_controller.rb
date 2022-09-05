79ca626e6da56697d60faaa7ceba81aafac232a3
ghostly08 <steven.produccionbcn@gmail.com>
2022-09-05 17:45:14 +0200

    nesting task routes in user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[new create]
 
   def index
     @tasks = Task.all
@@ -9,17 +10,22 @@ class TasksController < ApplicationController
   end
 
   def new
+    @user = User.find(params[:user_id])
     @task = Task.new
   end
 
   def create
-    task = Task.new(task_params)
-    task.save
-
-    redirect_to task_path(task)
+    @task = Task.new(task_params)
+    @task.user = @user
+    @task.save
+    redirect_to user_path(@user)
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:user_id])
+  end
 
   def task_params
     params.require(:task).permit(:name, :status)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show, :edit, :update]
-  resources :tasks do
-    get "tasks/:id", to: "tasks#show", as: :task
+  resources :users, only: [:index, :show, :edit, :update] do
+    resources :tasks
   end
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
