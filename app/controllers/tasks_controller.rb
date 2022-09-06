0047bbc5049a39cacca02accc19cbd1e60e170bb
ghostly08 <steven.produccionbcn@gmail.com>
2022-09-06 19:13:12 +0200

    implementing edit method on task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,18 @@ class TasksController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.user = @user
+    @task.update(task_params)
+
+    redirect_to user_path(@user)
+  end
+
   def destroy
     @task = Task.find(params[:id])
 
