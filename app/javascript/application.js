c846676d1fe1f78ab07df32a2ed4d349b9f37e1a
ghostly08 <steven.produccionbcn@gmail.com>
2022-09-10 02:43:17 +0200

    changing color on status

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,10 @@ import "./controllers"
 //= require jquery
 //= require jquery_ujs
 
+
+// Acces to the current class
+const element = document.getElementById("status").querySelectorAll(".status-color");
+orange = element[2].style.backgroundColor = "orange";
+// console.log(orange);
+
+// if Statement to change the current_class to the new one
