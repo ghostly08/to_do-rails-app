543109973007dd593d3ab9baa5d41913057c15aa
ghostly08 <steven.produccionbcn@gmail.com>
2022-09-11 00:13:38 +0200

    assigned colors on task status

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,11 +3,3 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 //= require jquery
 //= require jquery_ujs
-
-
-// Acces to the current class
-const element = document.getElementById("status").querySelectorAll(".status-color");
-orange = element[2].style.backgroundColor = "orange";
-// console.log(orange);
-
-// if Statement to change the current_class to the new one
